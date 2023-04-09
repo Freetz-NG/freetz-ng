812b3b462b473547877ce522b80aea675c5787b5
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-04-09 16:10:08 +0200

    Update 100-7362_7490.sh

--- patches/cond/100-aliens/scripts/100-7362_7490.sh
+++ patches/cond/100-aliens/scripts/100-7362_7490.sh
@@ -1,12 +1,42 @@
 isFreetzType 7362_7490 || return 0
+
+
+if [ -z "$FIRMWARE2" ]; then
+	echo "ERROR: no tk firmware" 1>&2
+	exit 1
+fi
+
 echo1 "adapt firmware for 7362"
 
+echo2 "copying 7362 wlan files"
+cp -a ${FILESYSTEM_TK_DIR}/etc/default.Fritz_Box_HW203/avm/wlan* ${FILESYSTEM_MOD_DIR}/etc/185/avm/
+
+modules=" net/ath_hal.ko"
+#modules=" net/ath_hal.ko net/aae.ko net/asf.ko"
+for i in $modules; do
+	cp -a "${FILESYSTEM_TK_DIR}/lib/modules/3.10.73/$i" "${FILESYSTEM_MOD_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/$i"
+done
+
+echo2 "copying 7362 dect files"
+file="lib/modules/dectfw_secondlevel_441.hex"
+cp -a "${FILESYSTEM_TK_DIR}/$file" "${FILESYSTEM_MOD_DIR}/$file"
+
+echo2 "copying 7362 webif files"
+#dsl
+[ "$FREETZ_AVM_VERSION_07_1X_MIN" == "y" ] && file="usr/www/avm/css/rd/illustrations/box.gif" || file="usr/www/avm/css/default/images/box.gif"
+cp -a "${FILESYSTEM_TK_DIR}/usr/www/avm/css/default/images/box.gif" "${FILESYSTEM_MOD_DIR}/$file"
+#dect
+[ "$FREETZ_AVM_VERSION_07_1X_MIN" == "y" ] && file="usr/www/avm/css/rd/illustrations/illu_dectFbox.png" || file="usr/www/avm/css/default/images/dect_fbox_icon.png"
+cp -a "${FILESYSTEM_TK_DIR}/usr/www/avm/css/default/images/dect_fbox_icon.png" "${FILESYSTEM_MOD_DIR}/$file"
 
 echo2 "moving default config dir"
-mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW185 \
-   ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW203
+mv ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW185 ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW203
 
 echo2 "patching rc.S and rc.conf"
+modsed -r 's/(CONFIG_ETH_COUNT)=.*$/\1="1"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+#modsed -r 's/(CONFIG_DECT_HOME)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
+#modsed -r 's/(CONFIG_HOME_AUTO)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
+#modsed -r 's/(CONFIG_HOME_AUTO_NET)=.*$/\1="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  #AHA?
 modsed 's/CONFIG_USB_XHCI=.*$/CONFIG_USB_XHCI="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_INSTALL_TYPE=.*$/CONFIG_INSTALL_TYPE="mips34_128MB_vdsl_dect441_2eth_2geth_1ab_2usb_host_wlan11n_10101"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_PRODUKT=.*$/CONFIG_PRODUKT="Fritz_Box_HW203"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
