d2e0b2112593cab088e0316127f7318e522bfbf4
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-04-11 18:31:15 +0200

    Update 100-7362_7490.sh

--- patches/cond/100-aliens/scripts/100-7362_7490.sh
+++ patches/cond/100-aliens/scripts/100-7362_7490.sh
@@ -19,8 +19,7 @@ oems="1und1 avm avme"
 for i in $oems; do
 	cp -a ${FILESYSTEM_TK_DIR}/etc/default.Fritz_Box_HW203/$i/wlan* ${FILESYSTEM_MOD_DIR}/etc/default.Fritz_Box_HW185/$i/
 done
-mkdir -p "${FILESYSTEM_MOD_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/net"
-cp -a "${FILESYSTEM_TK_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/net/*" "${FILESYSTEM_MOD_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/net/"
+cp -a -r "${FILESYSTEM_TK_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/net/*" "${FILESYSTEM_MOD_DIR}/lib/modules/${FREETZ_KERNEL_VERSION_MODULES_SUBDIR}/net/"
 
 echo2 "copying 7362 dect files"
 file="lib/modules/dectfw_secondlevel_441.hex"
