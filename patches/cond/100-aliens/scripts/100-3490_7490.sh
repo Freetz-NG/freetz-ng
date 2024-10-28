59b6cb41ea60847810ed32f47b1eac4a1c46f918
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-10-28 19:44:08 +0100

    expansion of the rc.conf patches

--- patches/cond/100-aliens/scripts/100-3490_7490.sh
+++ patches/cond/100-aliens/scripts/100-3490_7490.sh
@@ -25,10 +25,32 @@ else
 fi
 
 echo2 "patching rc.S and rc.conf"
-modsed 's/CONFIG_DECT=.*$/CONFIG_DECT="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed "s/\(CONFIG_.*MEDIA.*=\).*/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf" # Media
+modsed "s/\(CONFIG_.*MTD.*=\).*/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"   # MTD
+modsed "s/\(CONFIG_.*FAX.*=\).*/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"   # FAX
+modsed "s/\(CONFIG_.*DECT.*=\).*/\1\"n\"/" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"  # DECT
+
+# Other
+modsed 's/CONFIG_CONFIGSPACE_ONNAND=.*$/CONFIG_CONFIGSPACE_ONNAND="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_ONLINEPB=.*$/CONFIG_ONLINEPB="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_XILINX=.*$/CONFIG_XILINX="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# Remote
+modsed 's/CONFIG_T38=.*$/CONFIG_T38="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_TR369=.*$/CONFIG_TR369="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# Telephony
+modsed 's/CONFIG_AB_COUNT=.*$/CONFIG_AB_COUNT="0"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_CAPI_NT=.*$/CONFIG_CAPI_NT="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_CAPI_POTS=.*$/CONFIG_CAPI_POTS="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+modsed 's/CONFIG_CAPI_TE=.*$/CONFIG_CAPI_TE="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# AHA
 modsed 's/CONFIG_DECT_HOME=.*$/CONFIG_DECT_HOME="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_HOME_AUTO=.*$/CONFIG_HOME_AUTO="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_HOME_AUTO_NET=.*$/CONFIG_HOME_AUTO_NET="n"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
+
+# ProductInfo
 modsed 's/CONFIG_INSTALL_TYPE=.*$/CONFIG_INSTALL_TYPE="mips34_512MB_vdsl_4geth_2usb_host_offloadwlan11n_17525"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_PRODUKT=.*$/CONFIG_PRODUKT="Fritz_Box_HW212"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
 modsed 's/CONFIG_PRODUKT_NAME=.*$/CONFIG_PRODUKT_NAME="FRITZ!Box 3490"/g' "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.conf"
