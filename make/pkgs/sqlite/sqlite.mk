f8002d8211658491ab5f5b30343df51c257927c1
Ircama <Ircama>
2025-10-14 23:41:49 +0200

    PHP Library support

--- make/pkgs/sqlite/sqlite.mk
+++ make/pkgs/sqlite/sqlite.mk
@@ -1,13 +1,13 @@
-$(call PKG_INIT_BIN, $(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),3400100,3470100))
-$(PKG)_LIB_VERSION:=0.8.6
+$(call PKG_INIT_BIN, $(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),3400100,3500400))
+$(PKG)_LIB_VERSION:=3.50.4
 $(PKG)_SOURCE:=$(pkg)-autoconf-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH_ABANDON:=2c5dea207fa508d765af1ef620b637dcb06572afa6f01f0815bd5bbf864b33d9
-$(PKG)_HASH_CURRENT:=416a6f45bf2cacd494b208fdee1beda509abda951d5f47bc4f2792126f01b452
+$(PKG)_HASH_CURRENT:=a3db587a1b92ee5ddac2f66b3edb41b26f9c867275782d46c3a088977d6a5b18
 $(PKG)_HASH:=$($(PKG)_HASH_$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),ABANDON,CURRENT))
 $(PKG)_SITE_ABANDON:=https://www.sqlite.org/2022
-$(PKG)_SITE_CURRENT:=https://www.sqlite.org/2024
+$(PKG)_SITE_CURRENT:=https://www.sqlite.org/2025
 $(PKG)_SITE:=$($(PKG)_SITE_$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),ABANDON,CURRENT))
-### VERSION:=3.40.1/3.47.1
+### VERSION:=3.40.1/3.50.4
 ### WEBSITE:=https://www.sqlite.org
 ### MANPAGE:=https://www.sqlite.org/docs.html
 ### CHANGES:=https://www.sqlite.org/changes.html
@@ -19,11 +19,11 @@ endif
 
 $(PKG)_CONDITIONAL_PATCHES+=$(if $(FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON),abandon,current)
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/.libs/sqlite3
+$(PKG)_BINARY:=$($(PKG)_DIR)/sqlite3
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/sqlite3
 
-$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/.libs/libsqlite3.so.$($(PKG)_LIB_VERSION)
-$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsqlite3.so.$($(PKG)_LIB_VERSION)
+$(PKG)_LIB_BINARY:=$($(PKG)_DIR)/libsqlite3.so
+$(PKG)_LIB_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libsqlite3.so.$($(PKG)_LIB_VERSION)
 $(PKG)_LIB_TARGET_BINARY:=$($(PKG)_TARGET_LIBDIR)/libsqlite3.so.$($(PKG)_LIB_VERSION)
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libsqlite3_WITH_VERSION_ABANDON
@@ -35,6 +35,9 @@ $(PKG)_CONFIGURE_OPTIONS += --disable-static-shell
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_SQLITE_WITH_READLINE),--enable-readline,--disable-readline)
 
 $(PKG)_CONFIGURE_ENV += ac_cv_header_zlib_h=no
+
+DISABLE_NLS :=
+TARGET_CONFIGURE_OPTIONS :=
 
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -52,8 +55,7 @@ $($(PKG)_LIB_STAGING_BINARY): $($(PKG)_LIB_BINARY)
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		all install
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/sqlite3.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsqlite3.la
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/pkgconfig/sqlite3.pc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
@@ -68,10 +70,10 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_LIB_TARGET_BINARY)
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(SQLITE_DIR) clean
-	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libsqlite3* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/pkgconfig/sqlite3.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/sqlite \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/bin/sqlite3*
+	$(RM) -r $(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/libsqlite3* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/lib/pkgconfig/sqlite3.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/include/sqlite \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/local/bin/sqlite3*
 
 $(pkg)-uninstall:
 	$(RM) $(SQLITE_TARGET_BINARY) $(SQLITE_TARGET_LIBDIR)/libsqlite3*.so*
