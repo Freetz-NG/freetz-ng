6c2a7962bcf8e08c68e1768627f34496948e68a1
fda77 <fda77@users.noreply.github.com>
2025-11-08 00:49:11 +0100

    x

--- docs/stats/generate.sh
+++ docs/stats/generate.sh
@@ -13,7 +13,7 @@ table_head() {
 }
 
 spoiler_head() {
-	echo '<br />'
+	echo '<br>'
 	echo
 	echo '<details>'
 	echo -n '  <summary>'
@@ -47,7 +47,7 @@ get_hw() {
 	(
 		table_head
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(comment|config|bool) " | while read -r line; do
-			[ "${line#comment}" != "$line" ] &&  echo "$line"              | sed 's/^[^\t ]*[ \t]*"/| **/g;s/"/** |/g'
+			[ "${line#comment}" != "$line" ] &&  echo "$line"             | sed 's/^[^\t ]*[ \t]*"/| **/g;s/"/** |  |/g'
 			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n' | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
 			[ "${line#bool}"    != "$line" ] && echo "$line"              | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.hw.head"
 		done | sed 's/ - [^ ]*//g' | grep -Evi "(inhaus|labor|plus)"
@@ -56,7 +56,7 @@ get_hw() {
 
 # head
 (
-	echo '# Statistiken rund um Freetz-NG'  
+	echo '# Statistiken rund um Freetz-NG'
 	echo
 ) > "$OUTFILE"
 
