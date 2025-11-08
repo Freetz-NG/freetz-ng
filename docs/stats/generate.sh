9f2ab678325856a2ce42a05ff9bd2a9ad2529479
fda77 <fda77@users.noreply.github.com>
2025-11-08 04:11:53 +0100

    x

--- docs/stats/generate.sh
+++ docs/stats/generate.sh
@@ -10,12 +10,12 @@ rm -f "$TMPFILE"*
 SPACE='<!-- -->'
 
 empty_line() {
-	echo "| ${1:-$SPACE} | ${2:-$SPACE} |"
+	echo "@ ${1:-$SPACE} @ ${2:-$SPACE} @"
 }
 
 table_head() {
 	empty_line "$1" "$2"
-	echo '| --- | --- |'
+	echo '@ --- @ --- @'
 }
 
 spoiler_head() {
@@ -26,9 +26,8 @@ spoiler_head() {
 }
 
 spoiler_foot() {
-	cat "$1" |  sed -r 's, *\| (.*) \| (.*) \|,    | \2 | \1 |,g'
-#	cat "$1" |  sed -r 's, *\| (.*) \| (.*) \|,| \2 | \1 |,g'
-#	echo
+	cat "$1" | sed 's/|/\\|/g' | sed -r 's, *@ (.*) @ (.*) @,    | \2 | \1 |,g'
+#	cat "$1" | sed 's/|/\\|/g' | sed -r 's, *@ (.*) @ (.*) @,| \2 | \1 |,g'
 #	echo '</details>'
 	echo
 }
@@ -40,8 +39,8 @@ get_fw() {
 		table_head "Symbol" "Version"
 		empty_line
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(config|bool) " | while read -r line; do
-			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
-			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo > "$TMPFILE.fw.head"
+			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/@ /g;s/$/ @ /g'
+			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ @/g' && echo >> "$TMPFILE.fw.head"
 		done | sed 's/ - [^ ]*//g' | grep -Evi "(inhaus|labor|plus)"
 	) > "$TMPFILE.fw.body"
 }
@@ -52,11 +51,23 @@ get_hw() {
 	(
 		table_head "Symbol" "Name"
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(comment|config|bool) " | while read -r line; do
-			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed "s/^[^\t ]*[ \t]*\"/| $SPACE | **/g;s/\"/** |/g"
-			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
-			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo > "$TMPFILE.hw.head"
+			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed "s/^[^\t ]*[ \t]*\"/@ $SPACE @ **/g;s/\"/** @/g"
+			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/@ /g;s/$/ @ /g'
+			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ @/g' && echo >> "$TMPFILE.hw.head"
 		done | sed 's/ - [^ ]*//g'
 	) > "$TMPFILE.hw.body"
+}
+
+get_dl() {
+	area='Firmware source'
+	file="config/mod/dl-firmware.in"
+	(
+		table_head "Symbole" "Image"
+		empty_line
+		cat "$file" | grep "string \"${area}\"" -m1 -A9999 | grep "^config " -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(default) " | while read -r line; do
+			echo "$line" | tr -s ' ' | sed -r 's/.*"(.*)".* if (.*)/@ \2 @ \1 @/g' && echo >> "$TMPFILE.dl.head"
+		done | sed -r 's/_(inhaus|labor|plus)//gI' | grep -v "DETECT_IMAGE_NAME"
+	) > "$TMPFILE.dl.body"
 }
 
 main() {
@@ -76,6 +87,12 @@ main() {
 	spoiler_head "$TMPFILE.hw.head" "verschieden Geräte"
 	spoiler_foot "$TMPFILE.hw.body"
 	rm -f "$TMPFILE.hw."*
+
+	# image
+	get_dl
+	spoiler_head "$TMPFILE.dl.head" "verschieden Images"
+	spoiler_foot "$TMPFILE.dl.body"
+	rm -f "$TMPFILE.dl."*
 
 }
 main > "$OUTFILE"
