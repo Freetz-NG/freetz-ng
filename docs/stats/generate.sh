94a9135c7c7502134869dbd474512aa96d33f495
fda77 <fda77@users.noreply.github.com>
2025-11-08 01:29:47 +0100

    x

--- docs/stats/generate.sh
+++ docs/stats/generate.sh
@@ -8,8 +8,7 @@ rm -f "$TMPFILE"*
  
 
 empty_line() {
-#	echo '| <!-- --> | <!-- --> |'
-	echo '| &nbsp; | &nbsp; |'
+	echo '| <!-- --> | <!-- --> |'
 }
 
 table_head() {
@@ -22,7 +21,7 @@ spoiler_head() {
 	echo
 	echo '<details>'
 	echo -n '  <summary>'
-	echo -n "$(cat "$1" | wc -l  | tr -d '\n') $2"
+	echo -n "$(cat "$1" | wc -l | tr -d '\n') $2"
 	echo '</summary>'
 	echo
 }
@@ -52,7 +51,7 @@ get_hw() {
 	(
 		table_head
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(comment|config|bool) " | while read -r line; do
-			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed 's/^[^\t ]*[ \t]*"/| xx/g;s/"/xx | \&nbsp; |/g'
+			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed 's/^[^\t ]*[ \t]*"/| **/g;s/"/** | <!-- --> |/g'
 			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n' | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
 			[ "${line#bool}"    != "$line" ] && echo "$line"              | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.hw.head"
 		done | sed 's/ - [^ ]*//g' | grep -Evi "(inhaus|labor|plus)"
