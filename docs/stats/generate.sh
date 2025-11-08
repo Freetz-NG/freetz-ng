e9888c794e43eb3b03ac9ab8eec90137c7fb0600
fda77 <fda77@users.noreply.github.com>
2025-11-08 02:13:04 +0100

    x

--- docs/stats/generate.sh
+++ docs/stats/generate.sh
@@ -8,7 +8,7 @@ rm -f "$TMPFILE"*
 
 
 SPACE=' '
-SPACE='$nbsp;'
+SPACE='&nbsp;'
 SPACE='<!-- -->'
 
 empty_line() {
@@ -17,21 +17,21 @@ empty_line() {
 
 table_head() {
 	empty_line
-	echo '| -------- | -------- |'
+	echo '| --- | --- |'
 }
 
 spoiler_head() {
 	echo
 	echo '<p>'
-	echo '<details>'
+	echo '<details markdown>'
 	echo "    <summary>$(cat "$1" | wc -l | tr -d '\n') $2</summary>"
 	echo
 }
 
 spoiler_foot() {
 	cat "$1"
-#	echo
-	echo '</details>'
+	echo
+#	echo '</details>'
 	echo '</p>'
 	echo
 }
@@ -42,8 +42,8 @@ get_fw() {
 	(
 		table_head
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(config|bool) " | while read -r line; do
-			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n' | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
-			[ "${line#bool}"    != "$line" ] && echo "$line"              | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.fw.head"
+			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
+			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.fw.head"
 		done | sed 's/ - [^ ]*//g' | grep -Evi "(inhaus|labor|plus)"
 	) > "$TMPFILE.fw.body"
 }
@@ -54,10 +54,10 @@ get_hw() {
 	(
 		table_head
 		cat "$file" | grep "prompt \"${area}\"" -m1 -A9999 | grep "^endchoice" -m1 -B9999 | sed 's/^[ \t]*//g' | grep -E "^(comment|config|bool) " | while read -r line; do
-			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed 's/^[^\t ]*[ \t]*"/| **/g;s/"/** | $SPACE |/g'
-			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n' | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
-			[ "${line#bool}"    != "$line" ] && echo "$line"              | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.hw.head"
-		done | sed 's/ - [^ ]*//g' | grep -Evi "(inhaus|labor|plus)"
+			[ "${line#comment}" != "$line" ] && empty_line && echo "$line" | sed "s/^[^\t ]*[ \t]*"/| **/g;s/"/** | $SPACE |/g"
+			[ "${line#config}"  != "$line" ] && echo "$line" | tr -d '\n'  | sed 's/^[^\t ]*[ \t]*/| /g;s/$/ | /g'
+			[ "${line#bool}"    != "$line" ] && echo "$line"               | sed 's/^[^\t ]*[ \t]*"//g;s/"/ |/g' && echo >> "$TMPFILE.hw.head"
+		done | sed 's/ - [^ ]*//g'
 	) > "$TMPFILE.hw.body"
 }
 
